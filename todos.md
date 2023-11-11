b2686aec607a19b5e34db665bca062d8fd5179f9
Steffen Kowalski <scipper@myscipper.de>
2023-11-11 21:09:20 +0100

    restructured folders

---
+++ todos.md
@@ -0,0 +1,4 @@
+- input
+- player
+- objects
+- scene
---
+++ webui/src/main/ui/collectable/collectable-handler.ts
@@ -0,0 +1,21 @@
+import "./collectables.scss";
+
+export class CollectableHandler {
+
+  detectCollectableCollision(sheepPosition: { top: number; left: number },
+                             sheepDimensions: { width: number, height: number }) {
+    const collectables = [
+      ...document.elementsFromPoint(sheepPosition.left, sheepPosition.top),
+      ...document.elementsFromPoint(sheepPosition.left + sheepDimensions.width, sheepPosition.top),
+      ...document.elementsFromPoint(sheepPosition.left, sheepPosition.top + sheepDimensions.height),
+      ...document.elementsFromPoint(sheepPosition.left + sheepDimensions.width, sheepPosition.top + sheepDimensions.height)
+    ];
+    collectables.forEach((collectable) => {
+      if(collectable.classList.contains("collectable")) {
+        collectable.remove();
+      }
+    });
+
+  }
+
+}
